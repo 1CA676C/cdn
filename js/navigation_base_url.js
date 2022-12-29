38e5d9bacaa47948c7f2016667beaa216d88e291
Hbergamo <6856232+hsbergamo@users.noreply.github.com>
2022-12-28 22:28:27 -0300

    Update navigation_base_url.js

--- js/navigation_base_url.js
+++ js/navigation_base_url.js
@@ -1,1 +1,3 @@
- const navigationBaseURL = 'http://127.0.0.1:5500/'
+// const navigationBaseURL = 'http://127.0.0.1:5500/
+ 
+const navigationBaseURL = https://jogos.partita.mus.br/apps/arcade/musilingo/'
